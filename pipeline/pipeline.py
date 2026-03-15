3be6dac3c3add4bc38dae60688e1013ba9a3d425
basu16 <basusharma16@gmail.com>
2026-03-15 13:34:08 +0000

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,14 +1,12 @@
-import sys
-import pandas as pd 
+import sys 
+import pandas as pd
 
-print("arguments", sys.argv)
+print('arguments: ', sys.argv)
 
-month = int(sys.argv[1])
-
+month = sys.argv[1]
 df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
 df['month'] = month
 print(df.head())
 
 df.to_parquet(f"output_{month}.parquet")
-
-print(f"Hello pipeline, month =  {month}")
+print(f"hello pipeline, month = {month}")
