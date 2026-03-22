0102dd219ec0c1d47fe3d6c9c7bb37916f3b9203
basu16 <basusharma16@gmail.com>
2026-03-22 11:49:29 +0000

    commit for docker

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -11,5 +11,5 @@ df = pd.DataFrame({"day": [1, 2], "num_passengers  ": [3, 4]})
 df["month"] = month
 print(df.head())
 
-#df.to_parquet(f"output_month_{sys.argv[1]}.parquet")
+df.to_parquet(f"output_month_{sys.argv[1]}.parquet")
 print(f"Saving output for month {month}")
