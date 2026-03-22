20697a7fd4cdf690ff66b47a119c2de4c0eebdf5
basu16 <basusharma16@gmail.com>
2026-03-22 11:49:03 +0000

    new learning in docker

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,12 +1,15 @@
-import sys 
+import sys
 import pandas as pd
 
-print('arguments: ', sys.argv)
+print("arguments", sys.argv)
 
-month = sys.argv[1]
-df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
-df['month'] = month
+month = int(sys.argv[1])
+print(f"Running pipeline for month {month}")
+
+
+df = pd.DataFrame({"day": [1, 2], "num_passengers  ": [3, 4]})
+df["month"] = month
 print(df.head())
 
-df.to_parquet(f"output_{month}.parquet")
-print(f"hello pipeline, month = {month}")
+#df.to_parquet(f"output_month_{sys.argv[1]}.parquet")
+print(f"Saving output for month {month}")
